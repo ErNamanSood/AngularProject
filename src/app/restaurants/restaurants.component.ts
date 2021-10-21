3108662ae3e1022342e6dec8d59d463bf22e4b4f
ErNamanSood <namansoodns@gmail.com>
2021-10-21 13:13:40 +0530

    Save restaurant and list the data with toggle view

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Restaurant } from '../model/restaurant';
 import { RestaurantsService } from '../restaurants.service';
-import { getFirestore, collection, addDoc, setDoc, doc, Timestamp} from '@firebase/firestore/lite'
-import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL } from '@firebase/storage'
+import { getFirestore, collection, addDoc, getDocs, doc, Timestamp} from '@firebase/firestore/lite'
+import { getStorage, ref, uploadBytes, getDownloadURL } from '@firebase/storage'
 import { DBService } from '../db.service';
+
 
 @Component({
   selector: 'app-restaurants',
@@ -12,6 +13,7 @@ import { DBService } from '../db.service';
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
+
   /*restaurants: Restaurant[] = [
     new Restaurant("McDonalds", 30, 4.3, "burger, indian, continental"),
     new Restaurant("Table By Basant", 40, 4.5, "indian, chinese, continental"),
@@ -19,6 +21,9 @@ export class RestaurantsComponent implements OnInit {
   ];*/
 
   file: any;
+  restaurantList : any;
+  addView = false;
+  text = "Add Restaurant";
 
   cities = [
     {cityName: "Select City", state:""},
@@ -42,20 +47,30 @@ export class RestaurantsComponent implements OnInit {
     }
   );
 
- // restaurants = this.service.getRestaurants();
+  //restaurants = this.service.getRestaurants();
 
   // if the above array is suppose to be created by fetching data from Server
   // it will take time to load
-  // Injection for Service in Constructor of Component
 
+  // Injection for Service in Constructor of Component
   //constructor(private service: RestaurantsService) { }
-  constructor(private db: DBService){ }
+  constructor(private db: DBService) { 
+    this.fetchPromoRestaurants();
+  }
+  
 
   ngOnInit(): void {
   }
-  addRestaurant(name: string, timeToDeliver: string, ratings: string, categories: string){
 
+  addRestaurant(name: string, timeToDeliver: string, ratings: string, categories: string){
     //this.restaurants.push(new Restaurant(name, Number(timeToDeliver), Number(ratings), categories))
+  }
+
+  async fetchPromoRestaurants(){
+    const firestoreDB = getFirestore(this.db.app);
+    const promoCodeCollection = collection(firestoreDB, 'restaurants');
+    const snapshots = await getDocs(promoCodeCollection);
+    this.restaurantList = snapshots.docs.map(doc => doc.data());
   }
 
   pickFile(event:any){
@@ -65,24 +80,42 @@ export class RestaurantsComponent implements OnInit {
   }
 
   uploadImgeToFirebase(){
-    const metadata = {
-      contentType: 'image/jpeg',
-    };
-    
-    const storageReference = getStorage();
-
-    const restaurantImageReference = ref(storageReference, "restaurant-images/"+this.file.name);
+      const metadata = {
+        contentType: 'image/png',
+      };
+      const storageReference = getStorage();
+      const restaurantImageReference = ref(storageReference, "restaurant-images/"+this.file.name);
       uploadBytes(restaurantImageReference, this.file, metadata).then((snapshot) => {
        console.log("Image Uploaded Successfully");
        getDownloadURL(snapshot.ref).then((downloadURL) => {
         console.log('File available at', downloadURL);
-      });
-      });
-  }
+        // Save Restaurant Object in FirebaseFirestore
+        const dataToSave = this.restaurantForm.value;
+        // Save this in Firebase
+        dataToSave['imageUrl'] = downloadURL;
+        dataToSave['creationTime'] = Timestamp.now();
+        console.log("dataToSave is: "+dataToSave);
 
+        const firestoreDB = getFirestore(this.db.app);
+        
+        const restaurantCollection = collection(firestoreDB, 'restaurants');
+        addDoc(restaurantCollection, dataToSave);
+        console.log("Restaurant Added");
+        
+    })
+    .catch((error) =>{
+      console.log("Something Went Wrong");
+    }); 
+      });
+      
+
+      
+  }
+  
 
   addRestaurantToFirebase(){
     console.log(this.restaurantForm.value);
+  
     //1. Firstly, Upload the Image in Firebase Storage
     //   After Image is uploaded we will get image download URL
     //   in the form data, image FormControl value should be updated with download URL
@@ -91,6 +124,22 @@ export class RestaurantsComponent implements OnInit {
     // 1. Uplaod Image
     this.uploadImgeToFirebase();
 
-
+    // 2. Save the Data
+    // Divide the logic and execute the functions accordingly
   }
+
+  changeView(){
+    this.addView = !this.addView;
+    if(this.addView){
+      this.text = "View Restaurants";
+    }else{
+      this.text = "Add Restaurant";
+    }
+  }
+
 }
+
+// Tasks
+// 1. Divide the logic and execute the functions accordingly
+// 2. Reset the form after adding the restaurant
+// 3. Implement Loader | Circular Progress Indicator
